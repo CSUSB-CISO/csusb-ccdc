23efb1e7a1205c720225480cd105865ac3c1410f
l3m0n42 <Alberto.G.Yerena@gmail.com>
2024-01-17 20:15:05 -0800

    Updated entrypoint script

--- nifty-firewall-tool/start.sh
+++ nifty-firewall-tool/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Read environment variable
+API_KEY=${API_KEY:-default_api_key_value}
+
+# Update config.json with the API_KEY environment variable
+jq --arg api_key "$API_KEY" '.api_key = $api_key' /KeyboardKowboys/config.json > /tmp/temp.json && mv /tmp/temp.json /KeyboardKowboys/config.json
+
 # Apply nftables configuration
 nft -f nftables.conf
 
